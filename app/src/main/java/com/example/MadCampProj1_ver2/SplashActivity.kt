01e63dd12e7d8e0a62f513425bf9598bcf7d0fac
Lee Eunmin <dldmsals3@naver.com>
2025-07-09 17:30:59 +0900

    splash, icon 문제 해결

--- app/src/main/java/com/example/MadCampProj1_ver2/SplashActivity.kt
+++ app/src/main/java/com/example/MadCampProj1_ver2/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.example.MadCampProj1_ver2
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
@@ -21,17 +23,33 @@ class SplashActivity : AppCompatActivity() {
         // 애니메이션 비활성화
         overridePendingTransition(0, 0)
 
+
         val splashGif = findViewById<ImageView>(R.id.splashGif)
+
         Glide.with(this)
             .asGif()
-            .load(R.drawable.splash)
+            .load(R.drawable.splash2)
             .apply(
                 RequestOptions()
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE) // 디스크 캐싱 사용
-                    .override(800, 800) // 원본 크기로 디코딩
-                    .skipMemoryCache(true) // 메모리 캐시 비활성화
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                    .override(Target.SIZE_ORIGINAL)
+                    .skipMemoryCache(true)
             )
-            .into(splashGif)
+            .into(object : com.bumptech.glide.request.target.CustomTarget<com.bumptech.glide.load.resource.gif.GifDrawable>() {
+                override fun onResourceReady(
+                    resource: com.bumptech.glide.load.resource.gif.GifDrawable,
+                    transition: com.bumptech.glide.request.transition.Transition<in com.bumptech.glide.load.resource.gif.GifDrawable>?
+                ) {
+                    resource.setLoopCount(1) // 1회만 재생
+                    splashGif.setImageDrawable(resource)
+                    resource.start()
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    // 필요시 placeholder 설정
+                }
+            })
+
 
         // 일정 시간 후 MainActivity로 이동
         Handler(Looper.getMainLooper()).postDelayed({
